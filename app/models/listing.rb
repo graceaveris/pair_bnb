4855d4b9c3385fe8d7a7b2fe6e1e448961b6bdf4
graceaveris <graceaverisgmail.com>
2018-07-18 10:59:31 +0800

    a few tiny changes

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,5 @@ class Listing < ApplicationRecord
 	has_many :reservations, dependent: :destroy
 	belongs_to :user
 	mount_uploaders :images, ImageUploader
+	scope :max_guest_scope, -> (max_guest_number) { where(max_guest_number: max_guest_number) }
 end
